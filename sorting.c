2fc2c1e50a6c56b738af4b4d0c934b121a7a3904
luisnf5 <luisnun.fdez@gmail.com>
2024-10-21 16:33:30 +0200

    BubbleSortFlag()

--- sorting.c
+++ sorting.c
@@ -41,7 +41,31 @@ int BubbleSort(int* array, int ip, int iu)
 
 int BubbleSortFlag(int* array, int ip, int iu)
 {
-  /* your code */
+  int i, j, temp, flag;
+    int count = 0;  /**Contador de intercambios */
+
+    /* Validar los parámetros de entrada */
+    if (!array || ip < 0 || iu < 0 || ip > iu) return ERR;
+
+    /* Algoritmo de ordenación burbuja */
+    for (i = ip; i < iu; i++) {
+        flag = 0;
+        for (j = iu; j > i; j--) {
+            if (array[j] < array[j - 1]) {
+                temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+                count++;
+                flag = 1;
+            }
+        }
+
+        if (flag == 0){
+            break;
+        }
+    }
+
+    return count;
 }
 
 
