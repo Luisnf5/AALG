8ee39c38df03e988859b3cd4978acdbf5f1e534d
luisnf5 <luisnun.fdez@gmail.com>
2024-11-15 18:45:59 +0100

    no lo puto consigo

--- P2/permutations.c
+++ P2/permutations.c
@@ -11,6 +11,7 @@
 
 #include "permutations.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /***************************************************/
 /* Function: random_num Date:                      */
@@ -27,12 +28,8 @@
 /***************************************************/
 int random_num(int inf, int sup)
 {
-  if (inf > sup || inf < 0 || sup < 1)
-  {
-    return -1;
-  }
-
-  return rand() % (sup - inf) + inf;
+	printf("its me\n");
+	return inf + rand() % (sup - inf + 1);
 }
 
 /***************************************************/
@@ -50,39 +47,31 @@ int random_num(int inf, int sup)
 /***************************************************/
 int *generate_perm(int N)
 {
-  int *perm;
+	int *perm;
 
-  int i;
-  int cambio;
-  int ind;
-  int aux;
+	int i;
+	int cambio;
+	int ind;
+	int aux;
 
-  perm = (int *)malloc(N * sizeof(int));
-  if (perm == NULL)
-  {
-    return NULL;
-  }
+	perm = (int *)malloc(N * sizeof(int));
+	if (perm == NULL)
+	{
+		return NULL;
+	}
 
-  for (i = 1; i <= N; i++)
-  {
-    perm[i] = i;
-  }
+	for (i = 0; i < N; i++)
+	{
+		perm[i] = i + 1;
+	}
 
-  for (i = 0; i < N; i++)
-  {
-    cambio = perm[i];
-    aux = random_num(i, N);
-    if (aux == -1)
-    {
-      free(perm);
-      return NULL;
-    }
-    ind = aux;
-    perm[i] = perm[ind];
-    perm[ind] = cambio;
-  }
+	for (i = 0; i < N; i++)
+	{
+		aux = random_num(i, N - 1);
+		Swap(perm, i, aux);
+	}
 
-  return perm;
+	return perm;
 }
 
 /***************************************************/
@@ -102,45 +91,61 @@ int *generate_perm(int N)
 /***************************************************/
 int **generate_permutations(int n_perms, int N)
 {
-  int i, j;
-  int *aux;
+	int i, j;
+	int *aux;
 
-  int **permutations = malloc(n_perms * sizeof(int *));
-  if (permutations == NULL)
-  {
-    return NULL;
-  }
+	int **permutations = malloc(n_perms * sizeof(int *));
+	if (permutations == NULL)
+	{
+		return NULL;
+	}
 
-  for (i = 0; i < n_perms; i++)
-  {
-    aux = generate_perm(N);
-    if (aux == NULL)
-    {
-      for (j = 0; j < i; j++)
-      {
-        free(permutations[j]);
-      }
-      free(permutations);
-      return NULL;
-    }
-    permutations[i] = aux;
-  }
+	for (i = 0; i < n_perms; i++)
+	{
+		aux = generate_perm(N);
+		if (aux == NULL)
+		{
+			printf("Error en generate_perm\n");
+			for (j = 0; j < i; j++)
+			{
+				free(permutations[j]);
+			}
+			free(permutations);
+			return NULL;
+		}
+		permutations[i] = aux;
+	}
 
-  return permutations;
+	return permutations;
 }
 
 void free_permutations(int **permutations, int n_perms)
 {
-  int i;
+	int i;
 
-  if (permutations == NULL)
-  {
-    return;
-  }
+	if (permutations == NULL)
+	{
+		return;
+	}
 
-  for (i = 0; i < n_perms; i++)
-  {
-    free(permutations[i]);
-  }
-  free(permutations);
+	for (i = 0; i < n_perms; i++)
+	{
+		free(permutations[i]);
+	}
+	free(permutations);
+}
+
+int Swap(int *tabla, int i, int j)
+{
+	int temp;
+
+	printf("tablastart: i: %d, j: %d\n", tabla[i], tabla[j]);
+
+	temp = tabla[i];
+	tabla[i] = tabla[j];
+	tabla[j] = temp;
+
+	printf("tablaend: i: %d, j: %d\n", tabla[i], tabla[j]);
+
+	return 1;
 }
--- P2/sorting.c
+++ P2/sorting.c
@@ -10,6 +10,7 @@
  */
 
 #include "sorting.h"
+#include "permutations.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -20,319 +21,292 @@
 /***************************************************/
 int BubbleSort(int *array, int ip, int iu)
 {
-    int i, j, temp;
-    int count = 0; /**Contador de intercambios */
+	int i, j, temp;
+	int count = 0; /**Contador de intercambios */
 
-    /* Validar los parámetros de entrada */
-    if (!array || ip < 0 || iu < 0 || ip > iu)
-        return ERR;
+	/* Validar los parámetros de entrada */
+	if (!array || ip < 0 || iu < 0 || ip > iu)
+		return ERR;
 
-    /* Algoritmo de ordenación burbuja */
-    for (i = ip; i < iu; i++)
-    {
-        for (j = iu; j > i; j--)
-        {
-            if (array[j] < array[j - 1])
-            {
-                temp = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = temp;
-                count++;
-            }
-        }
-    }
+	/* Algoritmo de ordenación burbuja */
+	for (i = ip; i < iu; i++)
+	{
+		for (j = iu; j > i; j--)
+		{
+			if (array[j] < array[j - 1])
+			{
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				count++;
+			}
+		}
+	}
 
-    return count;
+	return count;
 }
 
 int BubbleSortFlag(int *array, int ip, int iu)
 {
-    int i, j, temp, flag;
-    int count = 0; /**Contador de intercambios */
+	int i, j, temp, flag;
+	int count = 0; /**Contador de intercambios */
 
-    /* Validar los parámetros de entrada */
-    if (!array || ip < 0 || iu < 0 || ip > iu)
-        return ERR;
+	/* Validar los parámetros de entrada */
+	if (!array || ip < 0 || iu < 0 || ip > iu)
+		return ERR;
 
-    /* Algoritmo de ordenación burbuja */
-    for (i = ip; i < iu; i++)
-    {
-        flag = 0;
-        for (j = iu; j > i; j--)
-        {
-            if (array[j] < array[j - 1])
-            {
-                temp = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = temp;
-                count++;
-                flag = 1;
-            }
-        }
+	/* Algoritmo de ordenación burbuja */
+	for (i = ip; i < iu; i++)
+	{
+		flag = 0;
+		for (j = iu; j > i; j--)
+		{
+			if (array[j] < array[j - 1])
+			{
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				count++;
+				flag = 1;
+			}
+		}
 
-        if (flag == 0)
-        {
-            break;
-        }
-    }
+		if (flag == 0)
+		{
+			break;
+		}
+	}
 
-    return count;
+	return count;
 }
 int mergesort(int *tabla, int ip, int iu)
 {
-    int imedio;
-    int ob = 0;
+	int imedio;
+	int ob = 0;
 
-    if (iu < ip || tabla == NULL || ip < 0 || iu < 0)
-    {
-        return ERR;
-    }
-    else if (iu == ip)
-    {
-        return 0;
-    }
+	if (iu < ip || tabla == NULL || ip < 0 || iu < 0)
+	{
+		return ERR;
+	}
+	else if (iu == ip)
+	{
+		return 0;
+	}
 
-    imedio = (ip + iu) / 2;
+	imedio = (ip + iu) / 2;
 
-    ob += mergesort(tabla, ip, imedio);
-    ob += mergesort(tabla, imedio + 1, iu);
-    ob += merge(tabla, ip, iu, imedio);
+	ob += mergesort(tabla, ip, imedio);
+	ob += mergesort(tabla, imedio + 1, iu);
+	ob += merge(tabla, ip, iu, imedio);
 
-    return ob;
+	return ob;
 }
 
 int merge(int *tabla, int ip, int iu, int imedio)
 {
-    int *tAux, i, j, k, ob = 0;
+	int *tAux, i, j, k, ob = 0;
 
-    if (iu < ip || tabla == NULL || ip < 0 || iu < 0 || imedio > iu || imedio < ip || imedio < 0)
-    {
-        return ERR;
-    }
+	if (iu < ip || tabla == NULL || ip < 0 || iu < 0 || imedio > iu || imedio < ip || imedio < 0)
+	{
+		return ERR;
+	}
 
-    tAux = (int *)malloc(((iu - ip) + 1) * sizeof(int));
+	tAux = (int *)malloc(((iu - ip) + 1) * sizeof(int));
 
-    if (tAux == NULL)
-    {
-        return ERR;
-    }
+	if (tAux == NULL)
+	{
+		return ERR;
+	}
 
-    for (i = ip; i <= iu; i++)
-    {
-        tAux[i - ip] = tabla[i];
-        ob++;
-    }
+	for (i = ip; i <= iu; i++)
+	{
+		tAux[i - ip] = tabla[i];
+		ob++;
+	}
 
-    i = 0;
-    j = imedio - ip + 1;
-    k = ip;
+	i = 0;
+	j = imedio - ip + 1;
+	k = ip;
 
-    while (i <= imedio - ip && j <= iu - ip)
-    {
-        ob++;
-        if (tAux[i] < tAux[j])
-        {
-            tabla[k] = tAux[i];
-            i++;
-        }
-        else
-        {
-            tabla[k] = tAux[j];
-            j++;
-        }
-        k++;
-        ob++;
-    }
+	while (i <= imedio - ip && j <= iu - ip)
+	{
+		ob++;
+		if (tAux[i] < tAux[j])
+		{
+			tabla[k] = tAux[i];
+			i++;
+		}
+		else
+		{
+			tabla[k] = tAux[j];
+			j++;
+		}
+		k++;
+		ob++;
+	}
 
-    if (i > imedio - ip)
-    {
-        while (j <= iu - ip)
-        {
-            tabla[k] = tAux[j];
-            j++;
-            k++;
-            ob++;
-        }
-    }
-    else if (j > iu - ip)
-    {
-        while (i <= imedio - ip)
-        {
-            tabla[k] = tAux[i];
-            i++;
-            k++;
-            ob++;
-        }
-    }
+	if (i > imedio - ip)
+	{
+		while (j <= iu - ip)
+		{
+			tabla[k] = tAux[j];
+			j++;
+			k++;
+			ob++;
+		}
+	}
+	else if (j > iu - ip)
+	{
+		while (i <= imedio - ip)
+		{
+			tabla[k] = tAux[i];
+			i++;
+			k++;
+			ob++;
+		}
+	}
 
-    free(tAux);
-    return ob;
+	free(tAux);
+	return ob;
+}
+
+int max(int *tabla, int n, int i, int j, int k, int *result)
+{
+
+	printf("i: %d, j: %d, k: %d\n", tabla[i], tabla[j], tabla[k]);
+	int ob = 0;
+	if (result == NULL || tabla == NULL || i > n || j > n || k > n)
+	{
+		printf("error\n");
+		return ERR;
+	}
+	if (tabla[i] > tabla[j])
+	{
+		ob++;
+		if (tabla[i] > tabla[k])
+		{
+			ob++;
+			*result = i;
+			return ob;
+		}
+		else
+		{
+			ob++;
+			*result = k;
+			return ob;
+		}
+	}
+	else
+	{
+		ob++;
+		if (tabla[j] > tabla[k])
+		{
+			ob++;
+			*result = j;
+			return ob;
+		}
+		else
+		{
+			ob++;
+			*result = k;
+			return ob;
+		}
+	}
 }
 
 int heapsort(int *tabla, int ip, int iu)
 {
-    int ob = 0, obaux;
+	printf("its m1\n");
+	int ob = 0;
+	int i;
+	int n = iu - ip + 1;
 
-    if (tabla == NULL || ip < 0 || iu < 0 || iu < ip)
-    {
-        printf("error 20\n");
-        return ERR;
-    }
+	printf("tabla init: ");
+	for (i = ip; i <= iu; i++)
+	{
+		printf("%d ", tabla[i]);
+	}
+	printf("\n\n");
 
-    obaux = CrearHeap(tabla, iu - ip + 1);
-    if (obaux == ERR)
-    {
-        printf("error 21\n");
-        return ERR;
-    }
-    else
-    {
-        ob += obaux;
-    }
+	ob += CrearHeap(tabla, n);
+	ob += OrdenarHeap(tabla, n);
 
-    obaux = OrdenarHeap(tabla, iu - ip + 1);
-    if (obaux == ERR)
-    {
-        printf("error 22\n");
-        return ERR;
-    }
-    else
-    {
-        ob += obaux;
-    }
+	printf("tabla end: ");
+	for (i = ip; i <= iu; i++)
+	{
+		printf("%d ", tabla[i]);
+	}
+	printf("\n\n");
 
-    return ob;
-}
-
-int Swap(int *tabla, int i, int j, int n)
-{
-    int temp;
-
-    if (tabla == NULL || i < 0 || j < 0 || i > n || j > n)
-    {
-        printf("error 23\n");
-        printf("i: %d, j: %d, n: %d\n", i, j, n);
-        return ERR;
-    }
-
-    if (i == j)
-    {
-        return 0;
-    }
-
-    temp = tabla[i];
-    tabla[i] = tabla[j];
-    tabla[j] = temp;
-
-    return 1;
-}
-
-int Heapify(int *tabla, int n, int i)
-{
-    int ob = 0, obaux;
-    if (tabla == NULL || n < 0 || i < 0 || i > n)
-    {
-        printf("error 24\n");
-        return ERR;
-    }
-
-    while ((2 * i) + 2 < n)
-    {
-        if (tabla[i] < tabla[2 * i + 1] && tabla[2 * i + 1] > tabla[2 * i + 2])
-        {
-            obaux = Swap(tabla, i, 2 * i + 1, n);
-            if (obaux == ERR)
-            {
-                printf("error 25\n");
-                return ERR;
-            }
-            else
-            {
-                ob += obaux;
-            }
-            i = 2 * i + 1;
-        }
-        else if (tabla[i] < tabla[2 * i + 2])
-        {
-            obaux = Swap(tabla, i, 2 * i + 2, n);
-            if (obaux == ERR)
-            {
-                printf("error 27\n");
-                return ERR;
-            }
-            else
-            {
-                ob += obaux;
-            }
-            i = 2 * i + 2;
-        }
-        else
-        {
-            break;
-        }
-    }
-    return ob;
-}
-
-int OrdenarHeap(int *tabla, int n)
-{
-    int i, temp, ind, ob = 0, obaux;
-
-    if (tabla == NULL || n < 1)
-    {
-        printf("error 28\n");
-        return ERR;
-    }
-
-    for (i = n - 1; i > 0; i--)
-    {
-        obaux = Swap(tabla, 0, i, n);
-        if (obaux == ERR)
-        {
-            printf("error 29\n");
-            return ERR;
-        }
-        else
-        {
-            ob += obaux;
-        }
-        obaux = Heapify(tabla, i, 0);
-        if (obaux == ERR)
-        {
-            printf("error 30\n");
-            return ERR;
-        }
-        else
-        {
-            ob += obaux;
-        }
-    }
-    return ob;
+	return ob;
 }
 
 int CrearHeap(int *tabla, int n)
 {
-    int i, ob = 0, obaux;
+	printf("its m2\n");
+	int i, ob = 0;
 
-    if (tabla == NULL || n < 1)
-    {
-        printf("error 31\n");
-        return ERR;
-    }
+	if (n == 1)
+	{
+		return ERR;
+	}
 
-    for (i = n / 2 - 1; i >= 0; i--)
-    {
-        obaux = Heapify(tabla, n, i);
-        if (obaux == ERR)
-        {
-            printf("error 32\n");
-            return ERR;
-        }
-        else
-        {
-            ob += obaux;
-        }
-    }
+	for (i = (n / 2) - 1; i >= 0; i--)
+	{
+		ob += Heapify(tabla, n, i);
+	}
 
-    return ob;
+	return ob;
+}
+
+int OrdenarHeap(int *tabla, int n)
+{
+	printf("its m3\n");
+	int i, ob = 0;
+
+	for (i = n - 1; i > 0; i--)
+	{
+		ob += Swap(tabla, 0, i);
+		ob += Heapify(tabla, i, 0);
+	}
+
+	return ob;
+}
+
+int Heapify(int *tabla, int n, int i)
+{
+	printf("its m4\n");
+	int ob = 0, indmax;
+
+	while (2 * i + 2 <= n)
+	{
+		ob += max(tabla, n, i, 2 * i + 1, 2 * i + 2, &indmax);
+		printf("indmax: %d\n", indmax);
+
+		if (indmax == i)
+		{
+			break;
+		}
+		else
+		{
+			ob += Swap(tabla, i, indmax);
+			i = indmax;
+		}
+	}
+
+	return ob;
+}
+
+int maxL(int *tabla, int n, int i, int j, int k, int *result)
+{
+	if (j < n && tabla[j] > tabla[i])
+	{
+		return j;
+	}
+	if (k < n && tabla[k] > tabla[i])
+	{
+		return k;
+	}
+
+	return i;
 }
